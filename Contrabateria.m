037798e33f3f3c797b18df2247560740a884deb0
lucas29mac <lucasmachadocbez@gmail.com>
2022-03-19 20:49:22 -0300

    Produçao de parabola aleatoria

--- Contrabateria.m
+++ Contrabateria.m
@@ -1,15 +1,27 @@
-clear all;
+clear;
 clc
 close all;
-parabola=randn(1,3);
-a=parabola(1);
-b=parabola(2);
-c=parabola(3);
 
-for x=1:1:10
 
-    y(x)=a*x^2+b*x+c;
+amostras=100;
 
-end
+x = linspace(-5, 5, amostras);
 
-plot(x,y);
+random=randn(1,10);
+
+a=random(1);
+b=random(2);
+
+
+y1 = a*x.^2+b;
+
+
+f = figure();
+
+ax = axes();
+
+hold(ax); 
+% to plot multiple lines on one axes
+
+plot(x, y1);
+
