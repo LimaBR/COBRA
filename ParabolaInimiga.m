19c83efdfa30d7ce24bd038a30ffeabda752a43e
Gabriel Lima <gabrielmartinslima@gmail.com>
2022-05-05 21:15:07 -0300

    RC2 para apresentação

--- ParabolaInimiga.m
+++ ParabolaInimiga.m
@@ -3,23 +3,23 @@ clc
 close all;
 %% Definição de constantes e coeficientes 
 
-amostras=100;
+amostras=1000;
 t = linspace(-50000, 50000, amostras);
 
 
 %elevaçao
-randomz=0.005*(randn(1,amostras)-0.5);
+randomz=0.002*(randn(1,amostras)-0.5);
 %distancia
 randomx=0.0005*(randn(1,amostras)-0.5);
 %azimute
-randomy=0.00009*(randn(1,amostras)-0.5);
+randomy=0.01*(randn(1,amostras)-0.5);
 
 random_other=randn(1,amostras);
 
 %ponto de origem do inimigo
 x0=abs(5000*random_other(3));
 y0=abs(5000*random_other(4));
-z0=abs(5000);
+z0=abs(10000);
 
 % a=-1*abs(random_other(1));
 % b=900000000000*abs(random_other(2));
